016ae47bfa03cd310fb73c3df8dd8500ca8c1e61
Смыков Сергей Сергеевич <SSmykov@nearmedic.ru>
2019-10-16 17:44:24 +0300

    'dict'

--- solution.py
+++ solution.py
@@ -1,27 +1,26 @@
-daysparty = list(map(int, input().split()))
-days = []
-for i in range(1, daysparty[0] + 1):
-    days.append(i)
-N = daysparty[1]
-# print(days)
-# print(party)
-party = []
-for i in range(N):
-    party.append(list(map(int, input().split())))
-# print(party)
-ddays = []
-for elem in party:
-    j = elem[0]
-    while j <= daysparty[0]:
-        ddays.append(j)
-        j += elem[1]
-# print(ddays)
-ddays = set(ddays)
-# print(ddays)
-holidays = []
-j = 6
-while j <= daysparty[0]:
-    ddays.discard(j)
-    ddays.discard(j + 1)
-    j += 7
-print(len(ddays))
+tel = input()
+
+
+def clean(tel):
+    tel = tel.replace('-', '')
+    tel = tel.replace('+7', '')
+    tel = tel.replace('(', '')
+    tel = tel.replace(')', '')
+    if len(tel) == 7:
+        tel = '495' + tel
+    elif len(tel) == 11:
+        tel = tel[1:]
+    return tel
+
+tel = clean(tel)
+book = []
+# print(tel)
+for i in range(3):
+    temp = input()
+    temp = clean(temp)
+    book.append(temp)
+for phone in book:
+    if phone == tel:
+        print('YES')
+    else:
+        print('NO')
