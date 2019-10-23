ad882438b7a34e181678685cf036da43152bb994
Смыков Сергей Сергеевич <SSmykov@nearmedic.ru>
2019-10-23 13:12:20 +0300

    '9week-beggining'

--- solution.py
+++ solution.py
@@ -1,26 +1,12 @@
-tel = input()
-
-
-def clean(tel):
-    tel = tel.replace('-', '')
-    tel = tel.replace('+7', '')
-    tel = tel.replace('(', '')
-    tel = tel.replace(')', '')
-    if len(tel) == 7:
-        tel = '495' + tel
-    elif len(tel) == 11:
-        tel = tel[1:]
-    return tel
-
-tel = clean(tel)
-book = []
-# print(tel)
-for i in range(3):
-    temp = input()
-    temp = clean(temp)
-    book.append(temp)
-for phone in book:
-    if phone == tel:
-        print('YES')
-    else:
-        print('NO')
+import itertools
+print(
+    *itertools.accumulate(
+        list(
+            map(
+                int, 
+                input().split()
+            )
+        ),
+        lambda x, y: x + y
+    )
+)
