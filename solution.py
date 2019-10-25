3fa36f56e013f53d7d33d205a7dbad9893f49834
Смыков Сергей Сергеевич <SSmykov@nearmedic.ru>
2019-10-25 13:22:26 +0300

    'complete-course'

--- solution.py
+++ solution.py
@@ -2,6 +2,12 @@ from copy import deepcopy
 from sys import stdin
 
 
+class MatrixError(BaseException):
+    def __init__(self, matrix, other):
+        self.matrix1 = matrix
+        self.matrix2 = other
+
+
 class Matrix:
     def __init__(self, matrix):
         self.matrix = deepcopy(matrix)
@@ -20,25 +26,49 @@ class Matrix:
         return(len(self.matrix), len(self.matrix[0]))
 
     def __add__(self, other):
-        temp = []
-        for i in range(len(self.matrix)):
-            temp2 = 0
-            temp3 = []
-            for j in range(len(self.matrix[i])):
-                temp2 = float(self.matrix[i][j]) + float(other.matrix[i][j])
-                temp3.append(int(temp2))
-            temp.append(temp3)
+        if (len(self.matrix), len(self.matrix[0])) == (len(other.matrix),
+                                                       len(other.matrix[0])):
+            temp = []
+            for i in range(len(self.matrix)):
+                temp2 = 0
+                temp3 = []
+                for j in range(len(self.matrix[i])):
+                    tmp2 = float(self.matrix[i][j]) + float(other.matrix[i][j])
+                    temp3.append(int(tmp2))
+                temp.append(temp3)
+        else:
+            error = MatrixError(self, other)
+            raise error
         return Matrix(temp)
 
     def __mul__(self, k):
         temp = []
         for i in range(len(self.matrix)):
-            temp2 = 0
             temp3 = []
             for j in range(len(self.matrix[i])):
                 temp3.append(self.matrix[i][j] * k)
             temp.append(temp3)
         return Matrix(temp)
+
+    @staticmethod
+    def transposed(self):
+        temp3 = []
+        for i in range(len(self.matrix[0])):
+            temp = []
+            for j in range(len(self.matrix)):
+                temp.append(self.matrix[j][i])
+            temp3.append(temp)
+        return Matrix(temp3)
+
+    def transpose(self):
+        temp3 = []
+        for i in range(len(self.matrix[0])):
+            temp = []
+            for j in range(len(self.matrix)):
+                temp.append(self.matrix[j][i])
+            temp3.append(temp)
+        self.matrix = deepcopy(temp3)
+        return Matrix(self.matrix)
 
     __rmul__ = __mul__
 
